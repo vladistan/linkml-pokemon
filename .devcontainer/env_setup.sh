3d93a970988ebc1c7d4e366a896e38fdbaf2d068
Vlad Korolev <vlad@v-lad.org>
2024-10-07 13:34:22 -0400

    Configure devcontainer

---
+++ .devcontainer/env_setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+
+STARTUP_DIR=$(pwd)
+
+
+python -m pip install -U pip
+
+
+# Clone linkml
+if [ ! -d "../linkml" ]; then
+  git clone https://github.com/vladistan/linkml.git ../linkml
+fi
+
+
+pipx install pre-commit
+pipx install poetry
+
+echo "PWD: $PWD"
+
+if [[ "$PWD" == *"/workspaces/"* ]]; then
+  echo "Running in VSCode Dev Container"
+elif [[ "$PWD" == *"/IdeaProjects/"* ]]; then
+  echo "Running in JetBrains IDE"
+fi
+
+# Configure LinkML
+pre-commit install || true
+poetry install --with-extras
+pre-commit run -a || true
+# Configure Pokemon
+cd "${STARTUP_DIR}"
+poetry install --with-extras
+pip install -e ../linkml
+
---
+++ .devcontainer/post_attach.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Running post attach script"
+# Run post attach
+code --add ../linkml/
